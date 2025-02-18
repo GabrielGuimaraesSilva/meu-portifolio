91a73e05ba262c125656ad4cde5a3ad51514d68a
GabrielGuimaraesSilva <gabrielguimaraes201450@gmail.com>
2025-02-17 21:59:36 -0300

    Adicionando seção conhecimentos

---
+++ script.js
@@ -0,0 +1,7 @@
+function showDescription(text) {
+    document.getElementById("tech-description").textContent = text;
+}
+
+function resetDescription() {
+    document.getElementById("tech-description").textContent = "Passe o cursor do mouse no card para ler";
+}
